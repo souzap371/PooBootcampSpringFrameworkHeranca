2bc0f98d6877bc88d50efd603986f2da74fdefb4
Pablo Souza <souzap371@gmail.com>
2022-07-22 06:02:21 -0300

    Criando Classe Veiculo e heranças Moto, Carro e caminhao

---
+++ src/Moto.java
@@ -0,0 +1,8 @@
+/**
+ * Classe de exemplo para o exercício da Aula 4 de Orientação a Objetos.
+ */
+//Classe Moto sendo herança da classe Veiculo
+
+public class Moto extends Veiculo {
+
+}
---
+++ src/Veiculo.java
@@ -0,0 +1,8 @@
+/**
+ * Classe de exemplo para o exercício da Aula 4 de Orientação a Objetos
+ */
+
+public class Veiculo {
+
+    //Atributos e métodos
+}
